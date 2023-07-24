0ac5db04d7e66e99e0ec766b252027e21d9c942e
kacperjanus <kacperjanus8@gmail.com>
2023-07-24 22:43:42 +0200

    Clean up fetch requests when searching for movies

--- use-popcorn/src/App.js
+++ use-popcorn/src/App.js
@@ -77,21 +77,24 @@ export default function App() {
 
 	useEffect(
 		function () {
+			const controller = new AbortController();
 			async function fetchMovies() {
 				try {
 					setError("");
 					setIsLoading(true);
 					const res = await fetch(
-						`http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
+						`http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+						{ signal: controller.signal }
 					);
 					if (!res.ok) throw new Error("Something went wrong!");
 					const data = await res.json();
 
 					if (!data.Search) throw new Error("No movies found");
 					setMovies(data.Search);
+					setError("");
 					setIsLoading(false);
 				} catch (err) {
-					setError(err.message);
+					if (err.name !== "AbortError") setError(err.message);
 				}
 			}
 			if (query.length < 3) {
@@ -100,6 +103,10 @@ export default function App() {
 				return;
 			}
 			fetchMovies();
+
+			return function () {
+				controller.abort();
+			};
 		},
 		[query]
 	);
