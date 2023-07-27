c2643a3d67de165164acafed7ded1d6c4effe4f2
kacperjanus <kacperjanus8@gmail.com>
2023-07-27 20:54:33 +0200

    Creating static version of the app with questions loaded from fake api

--- react-quiz/src/App.js
+++ react-quiz/src/App.js
@@ -1,9 +1,41 @@
-import DateCounter from "./DateCounter";
+import { useEffect, useReducer } from "react";
+import Header from "./Header.js";
+import Main from "./Main.js";
+
+const initialState = {
+	questions: [],
+	//loading, error, ready, active, finished
+	status: "loading",
+};
+
+function reducer(state, action) {
+	switch (action.type) {
+		case "dataReceived":
+			return { ...state, questions: action.payload, status: "ready" };
+		case "dataFailed":
+			return { ...state, status: "error" };
+		default:
+			throw new Error("Action unknown");
+	}
+}
 
 export default function App() {
+	const [state, dispatch] = useReducer(reducer, initialState);
+
+	useEffect(function () {
+		fetch("http://localhost:8000/questions")
+			.then((res) => res.json)
+			.then((data) => dispatch({ type: "dataReceived", payload: data }))
+			.catch((err) => dispatch({ type: "dataFailed" }));
+	}, []);
 	return (
-		<div>
-			<DateCounter />
+		<div className="app">
+			<Header />
+
+			<Main>
+				<p>1/15</p>
+				<p>Question</p>
+			</Main>
 		</div>
 	);
 }
