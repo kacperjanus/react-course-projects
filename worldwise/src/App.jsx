5fd52d901b7b7d7cf74af16adaa11d53fff747ad
kacperjanus <kacperjanus8@gmail.com>
2023-07-30 13:33:45 +0200

    Importing starter files

--- worldwise/src/App.jsx
+++ worldwise/src/App.jsx
@@ -2,7 +2,10 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import Homepage from "./pages/Homepage";
+import AppLayout from "./pages/AppLayout";
 import PageNotFound from "./pages/PageNotFound";
+import Login from "./pages/Login";
+import "./index.css";
 
 function App() {
 	return (
@@ -10,6 +13,8 @@ function App() {
 			<Routes>
 				<Route path="product" element={<Product />} />
 				<Route path="pricing" element={<Pricing />} />
+				<Route path="app" element={<AppLayout />} />
+				<Route path="login" element={<Login />} />
 				<Route path="/" element={<Homepage />} />
 				<Route path="*" element={<PageNotFound />} />
 			</Routes>
