43b3ba7221198b600dff2a1f53d74a30a0938fb0
kacperjanus <kacperjanus8@gmail.com>
2023-07-30 22:58:42 +0200

    Introducing ContextAPI to manage app state

--- worldwise/src/components/CityList.jsx
+++ worldwise/src/components/CityList.jsx
@@ -2,11 +2,14 @@ import Spinner from "./Spinner.jsx";
 import styles from "./CityList.module.css";
 import CityItem from "./CityItem.jsx";
 import Message from "./Message.jsx";
+import { useCities } from "../contexts/CitiesContext.jsx";
 
-function CityList({ cities, isLoading }) {
+function CityList() {
+	const { cities, isLoading } = useCities();
 	if (isLoading) return <Spinner />;
 	if (cities.length === 0)
 		return <Message message={"No cities to display"} />;
+
 	return (
 		<ul className={styles.cityList}>
 			{cities.map((city) => (
