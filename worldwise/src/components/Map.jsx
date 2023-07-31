3286368091eb080c9b34fa983f162d15ece97b4e
kacperjanus <kacperjanus8@gmail.com>
2023-07-31 21:14:44 +0200

    Implemented displaying map in App view using Leaflet library

--- worldwise/src/components/Map.jsx
+++ worldwise/src/components/Map.jsx
@@ -1,17 +1,71 @@
 import { useNavigate, useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
+import {
+	MapContainer,
+	TileLayer,
+	Marker,
+	Popup,
+	useMap,
+	useMapEvent,
+} from "react-leaflet";
+import { useEffect, useState } from "react";
+import { useCities } from "../contexts/CitiesContext";
 
 function Map() {
-	const navigate = useNavigate();
-	const [searchParams, setSearchParams] = useSearchParams();
+	const { cities } = useCities();
+	const [searchParams] = useSearchParams();
+
+	const [mapPosition, setMapPosition] = useState([0, 0]);
 	const lat = searchParams.get("lat");
 	const lng = searchParams.get("lng");
 
+	useEffect(
+		function () {
+			if (lat && lng) setMapPosition([lat, lng]);
+		},
+		[lat, lng]
+	);
+
 	return (
-		<div className={styles.mapContainer} onClick={() => navigate("form")}>
-			Map
+		<div className={styles.mapContainer}>
+			<MapContainer
+				center={mapPosition}
+				zoom={13}
+				scrollWheelZoom={true}
+				className={styles.mapContainer}
+			>
+				<TileLayer
+					attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+					url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+				/>
+				{cities.map((city) => (
+					<Marker
+						key={city.id}
+						position={[city.position.lat, city.position.lng]}
+					>
+						<Popup className="leaflet-popup">
+							{city.emoji} {city.cityName}
+						</Popup>
+					</Marker>
+				))}
+				<ChangeCenter position={mapPosition} />
+				<DetectClick />
+			</MapContainer>
 		</div>
 	);
+}
+
+function ChangeCenter({ position }) {
+	const map = useMap();
+	map.setView(position);
+	return null;
+}
+
+function DetectClick() {
+	const navigate = useNavigate();
+	useMapEvent({
+		click: (e) => navigate(`form?lat=${e.latlng.lat}&lng=${e.latlng.lng}`),
+	});
 }
 
 export default Map;
