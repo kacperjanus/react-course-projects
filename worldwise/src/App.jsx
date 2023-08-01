a5aaa5464039848db3321f9278d0a910540290f2
kacperjanus <kacperjanus8@gmail.com>
2023-08-01 22:03:06 +0200

    Implemented logging user in with new FakeAuthContext

--- worldwise/src/App.jsx
+++ worldwise/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { CitiesProvider } from "./contexts/CitiesContext";
+import { AuthProvider } from "./contexts/FakeAuthContext";
 
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
@@ -17,25 +18,27 @@ import "./index.css";
 function App() {
 	return (
 		<CitiesProvider>
-			<BrowserRouter>
-				<Routes>
-					<Route path="product" element={<Product />} />
-					<Route path="pricing" element={<Pricing />} />
-					<Route path="app" element={<AppLayout />}>
-						<Route
-							index
-							element={<Navigate replace to="cities" />}
-						/>
-						<Route path="cities" element={<CityList />} />
-						<Route path="cities/:id" element={<City />} />
-						<Route path="countries" element={<CountryList />} />
-						<Route path="form" element={<Form />} />
-					</Route>
-					<Route path="login" element={<Login />} />
-					<Route path="/" element={<Homepage />} />
-					<Route path="*" element={<PageNotFound />} />
-				</Routes>
-			</BrowserRouter>
+			<AuthProvider>
+				<BrowserRouter>
+					<Routes>
+						<Route path="product" element={<Product />} />
+						<Route path="pricing" element={<Pricing />} />
+						<Route path="app" element={<AppLayout />}>
+							<Route
+								index
+								element={<Navigate replace to="cities" />}
+							/>
+							<Route path="cities" element={<CityList />} />
+							<Route path="cities/:id" element={<City />} />
+							<Route path="countries" element={<CountryList />} />
+							<Route path="form" element={<Form />} />
+						</Route>
+						<Route path="login" element={<Login />} />
+						<Route path="/" element={<Homepage />} />
+						<Route path="*" element={<PageNotFound />} />
+					</Routes>
+				</BrowserRouter>
+			</AuthProvider>
 		</CitiesProvider>
 	);
 }
--- worldwise/src/pages/Login.jsx
+++ worldwise/src/pages/Login.jsx
@@ -1,16 +1,33 @@
 import styles from "./Login.module.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import PageNav from "../components/PageNav";
+import { useAuth } from "../contexts/FakeAuthContext";
+import { useNavigate } from "react-router-dom";
+import Button from "../components/Button";
 
 export default function Login() {
 	// PRE-FILL FOR DEV PURPOSES
 	const [email, setEmail] = useState("jack@example.com");
 	const [password, setPassword] = useState("qwerty");
+	const { logIn, isAuthenticated } = useAuth();
+	const navigate = useNavigate();
+
+	useEffect(
+		function () {
+			if (isAuthenticated) navigate("/app", { replace: true });
+		},
+		[isAuthenticated, navigate]
+	);
+
+	function handleSubmit(e) {
+		e.preventDefault();
+		if (email && password) logIn(email, password);
+	}
 
 	return (
 		<main className={styles.login}>
 			<PageNav />
-			<form className={styles.form}>
+			<form className={styles.form} onSubmit={handleSubmit}>
 				<div className={styles.row}>
 					<label htmlFor="email">Email address</label>
 					<input
@@ -32,7 +49,7 @@ export default function Login() {
 				</div>
 
 				<div>
-					<button>Login</button>
+					<Button type={"primary"}>Log in</Button>
 				</div>
 			</form>
 		</main>
