9003394cb26f90abfd4f2524038fbdc2f0b3401e
kacperjanus <kacperjanus8@gmail.com>
2023-08-08 17:50:18 +0200

    Added ability to place order and submit them to API

--- fast-react-pizza/src/App.jsx
+++ fast-react-pizza/src/App.jsx
@@ -3,11 +3,12 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Home from "./ui/Home";
 import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
-import CreateOrder from "./features/order/CreateOrder";
-import Order from "./features/order/Order";
+import CreateOrder, {
+	action as createOrderAction,
+} from "./features/order/CreateOrder";
+import Order, { loader as orderLoader } from "./features/order/Order";
 import AppLayout from "./ui/AppLayout";
 import Error from "./ui/Error.jsx";
-import { loader as orderLoader } from "./features/order/Order";
 
 const router = createBrowserRouter([
 	{
@@ -31,6 +32,7 @@ const router = createBrowserRouter([
 			{
 				path: "/order/new",
 				element: <CreateOrder />,
+				action: createOrderAction,
 			},
 			{
 				path: "/order/:orderId",
