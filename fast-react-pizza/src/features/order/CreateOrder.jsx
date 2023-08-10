882863c6ec367ee6db94b49f04196a6ce8f49ee9
kacperjanus <kacperjanus8@gmail.com>
2023-08-10 19:57:53 +0200

    Importing username value from Redux store

--- fast-react-pizza/src/features/order/CreateOrder.jsx
+++ fast-react-pizza/src/features/order/CreateOrder.jsx
@@ -1,6 +1,7 @@
 import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
 import { createOrder } from "../../services/apiRestaurant";
 import Button from "../../ui/Button";
+import { useSelector } from "react-redux";
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -41,6 +42,8 @@ function CreateOrder() {
 
     const formErrors = useActionData();
 
+    const username = useSelector((state) => state.user.username);
+
     return (
         <div className="px-4 py-6">
             <h2 className="mb-8 text-xl font-semibold">
@@ -54,6 +57,7 @@ function CreateOrder() {
                         className="input grow"
                         type="text"
                         name="customer"
+                        defaultValue={username}
                         required
                     />
                 </div>
