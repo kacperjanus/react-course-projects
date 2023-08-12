000aa1c496207ea0df26cbdfa54c916d3bcc4b58
kacperjanus <kacperjanus8@gmail.com>
2023-08-12 17:24:29 +0200

    Fetching user's geolocation when placing the order

--- fast-react-pizza/src/features/order/CreateOrder.jsx
+++ fast-react-pizza/src/features/order/CreateOrder.jsx
@@ -2,11 +2,12 @@ import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
 import { createOrder } from "../../services/apiRestaurant";
 import Button from "../../ui/Button";
 import EmptyCart from "../cart/EmptyCart";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { clearCart, getCart, getTotalCartPrice } from "../cart/cartSlice";
 import store from "../../store";
 import { formatCurrency } from "../../utils/helpers";
 import { useState } from "react";
+import { fetchAddress } from "../user/userSlice";
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -19,9 +20,18 @@ function CreateOrder() {
     const cart = useSelector(getCart);
     const navigation = useNavigation();
     const formErrors = useActionData();
-    const username = useSelector((state) => state.user.username);
+    const {
+        username,
+        address,
+        position,
+        status: addressStatus,
+        error: errorAddress,
+    } = useSelector((state) => state.user);
     const totalCartPrice = useSelector(getTotalCartPrice);
     const priorityPrice = withPriority ? Number(totalCartPrice) * 0.2 : 0;
+    const dispatch = useDispatch();
+
+    const isLoading = addressStatus === "loading";
 
     if (!cart.length) return <EmptyCart />;
 
@@ -62,15 +72,35 @@ function CreateOrder() {
                     </div>
                 </div>
 
-                <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+                <div className="relative mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
                     <label className="sm:basis-40">Address</label>
                     <div className="grow">
                         <input
                             className="input w-full"
                             name="address"
                             required
+                            defaultValue={address}
                         />
+                        {addressStatus === "error" && (
+                            <p className="mt-2 rounded-full bg-red-100 p-2 text-xs text-red-700">
+                                {errorAddress}
+                            </p>
+                        )}
                     </div>
+                    <span className="absolute right-[5px] top-[5px] z-50">
+                        {!position.longitude && !position.latitude && (
+                            <Button
+                                type="small"
+                                onClick={(e) => {
+                                    e.preventDefault();
+                                    dispatch(fetchAddress());
+                                }}
+                                disabled={isLoading}
+                            >
+                                Get position
+                            </Button>
+                        )}
+                    </span>
                 </div>
 
                 <div className="mb-12 flex items-center gap-5">
@@ -93,7 +123,16 @@ function CreateOrder() {
                         name="cart"
                         value={JSON.stringify(cart)}
                     />
-                    <Button type="primary" disabled={isSubmitting}>
+                    <input
+                        type="hidden"
+                        name="position"
+                        value={
+                            position.longitude && position.latitude
+                                ? `${position.latitude}, ${position.longitude}`
+                                : ""
+                        }
+                    />
+                    <Button type="primary" disabled={isSubmitting || isLoading}>
                         Order now{" "}
                         {formatCurrency(Number(totalCartPrice) + priorityPrice)}
                     </Button>
