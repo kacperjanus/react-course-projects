c4706312f103dd52acffd3b00806465f5d11bfc8
kacperjanus <kacperjanus8@gmail.com>
2023-08-13 14:45:02 +0200

    Setting up fetching data about cabins with Ract Query

--- the-wild-oasis/src/pages/Cabins.jsx
+++ the-wild-oasis/src/pages/Cabins.jsx
@@ -1,13 +1,18 @@
+import CabinTable from "../features/cabins/CabinTable";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 
 function Cabins() {
-  return (
-    <Row type="horizontal">
-      <Heading as="h1">All cabins</Heading>
-      <p>TEST</p>
-    </Row>
-  );
+    return (
+        <>
+            <Row type="horizontal">
+                <Heading as="h1">All cabins</Heading>
+                <p>Filter/sort</p>
+            </Row>
+
+            <CabinTable />
+        </>
+    );
 }
 
 export default Cabins;
