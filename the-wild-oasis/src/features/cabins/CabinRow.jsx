69de3f6ea475987dc07d5a109a32fcc00bd9df88
kacperjanus <kacperjanus8@gmail.com>
2023-08-13 15:05:00 +0200

    Implemented programatic way to delete a cabin

--- the-wild-oasis/src/features/cabins/CabinRow.jsx
+++ the-wild-oasis/src/features/cabins/CabinRow.jsx
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 import { formatCurrency } from "../../utils/helpers";
+import { deleteCabin } from "../../services/apiCabins";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 const TableRow = styled.div`
     display: grid;
@@ -41,7 +43,27 @@ const Discount = styled.div`
 `;
 
 function CabinRow({ cabin }) {
-    const { name, maxCapacity, regularPrice, discount, image } = cabin;
+    const {
+        id: cabinId,
+        name,
+        maxCapacity,
+        regularPrice,
+        discount,
+        image,
+    } = cabin;
+
+    const queryClient = useQueryClient();
+
+    const { isLoading, mutate } = useMutation({
+        mutationFn: deleteCabin,
+        onSuccess: () => {
+            alert("Cabin successfully deleted");
+            queryClient.invalidateQueries({
+                queryKey: ["cabins"],
+            });
+        },
+        onError: (err) => alert(err.message),
+    });
     return (
         <TableRow>
             <Img src={image} />
@@ -49,7 +71,9 @@ function CabinRow({ cabin }) {
             <div>Fits up to {maxCapacity} people</div>
             <Price>{formatCurrency(regularPrice)}</Price>
             <Discount>{formatCurrency(discount)}</Discount>
-            <button>Delete</button>
+            <button disabled={isLoading} onClick={() => mutate(cabinId)}>
+                Delete
+            </button>
         </TableRow>
     );
 }
