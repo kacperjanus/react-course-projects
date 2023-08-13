040ceedc7bb9035c6d5505862e4667d6a13d086c
kacperjanus <kacperjanus8@gmail.com>
2023-08-13 14:45:31 +0200

    Setting up Supabase

---
+++ the-wild-oasis/src/services/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://nfyipzromfvdtbcveygq.supabase.co";
+const supabaseKey =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5meWlwenJvbWZ2ZHRiY3ZleWdxIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTE4NzAyNTQsImV4cCI6MjAwNzQ0NjI1NH0.0G5bpn6knaY8hkgzLrv22ITmMRz2sf6uqIJsmiE6b3k";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
