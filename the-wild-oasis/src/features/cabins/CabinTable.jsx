06ef9ac5794357c9c1ad44f8dbd6c6e8ff3e3aea
kacperjanus <kacperjanus8@gmail.com>
2023-08-20 16:08:28 +0200

    Displaying bookings table in bookings page

--- the-wild-oasis/src/features/cabins/CabinTable.jsx
+++ the-wild-oasis/src/features/cabins/CabinTable.jsx
@@ -5,6 +5,7 @@ import CabinRow from "./CabinRow";
 import Table from "../../ui/Table";
 import Menus from "../../ui/Menus";
 import { useSearchParams } from "react-router-dom";
+import Empty from "../../ui/Empty";
 
 function CabinTable() {
     const { isLoading, cabins } = useCabins();
@@ -29,6 +30,8 @@ function CabinTable() {
     );
 
     if (isLoading) return <Spinner />;
+
+    if (!cabins.length) return <Empty resourceName="cabins" />;
 
     return (
         <Menus>
