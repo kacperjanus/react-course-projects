323f0242936b2d092f9ac00e3fbdef3115b59cfb
kacperjanus <kacperjanus8@gmail.com>
2023-08-22 00:01:02 +0200

    Creating booking detail page

--- the-wild-oasis/src/features/bookings/BookingDetail.jsx
+++ the-wild-oasis/src/features/bookings/BookingDetail.jsx
@@ -9,44 +9,52 @@ import Button from "../../ui/Button";
 import ButtonText from "../../ui/ButtonText";
 
 import { useMoveBack } from "../../hooks/useMoveBack";
+import { useParams } from "react-router-dom";
+import { useBooking } from "./useBooking";
+import Spinner from "../../ui/Spinner";
 
 const HeadingGroup = styled.div`
-  display: flex;
-  gap: 2.4rem;
-  align-items: center;
+    display: flex;
+    gap: 2.4rem;
+    align-items: center;
 `;
 
 function BookingDetail() {
-  const booking = {};
-  const status = "checked-in";
+    const { booking, isLoading } = useBooking();
+    const status = "checked-in";
 
-  const moveBack = useMoveBack();
+    const moveBack = useMoveBack();
 
-  const statusToTagName = {
-    unconfirmed: "blue",
-    "checked-in": "green",
-    "checked-out": "silver",
-  };
+    const statusToTagName = {
+        unconfirmed: "blue",
+        "checked-in": "green",
+        "checked-out": "silver",
+    };
 
-  return (
-    <>
-      <Row type="horizontal">
-        <HeadingGroup>
-          <Heading as="h1">Booking #X</Heading>
-          <Tag type={statusToTagName[status]}>{status.replace("-", " ")}</Tag>
-        </HeadingGroup>
-        <ButtonText onClick={moveBack}>&larr; Back</ButtonText>
-      </Row>
+    if (isLoading) return <Spinner />;
 
-      <BookingDataBox booking={booking} />
+    const { id } = booking;
+    return (
+        <>
+            <Row type="horizontal">
+                <HeadingGroup>
+                    <Heading as="h1">Booking #{id}</Heading>
+                    <Tag type={statusToTagName[status]}>
+                        {status.replace("-", " ")}
+                    </Tag>
+                </HeadingGroup>
+                <ButtonText onClick={moveBack}>&larr; Back</ButtonText>
+            </Row>
 
-      <ButtonGroup>
-        <Button variation="secondary" onClick={moveBack}>
-          Back
-        </Button>
-      </ButtonGroup>
-    </>
-  );
+            <BookingDataBox booking={booking} />
+
+            <ButtonGroup>
+                <Button $variation="secondary" onClick={moveBack}>
+                    Back
+                </Button>
+            </ButtonGroup>
+        </>
+    );
 }
 
 export default BookingDetail;
---
+++ the-wild-oasis/src/features/bookings/useBooking.js
@@ -0,0 +1,17 @@
+import { useQuery } from "@tanstack/react-query";
+import { useParams } from "react-router-dom";
+import { getBooking } from "../../services/apiBookings";
+
+export function useBooking() {
+    const { bookingId } = useParams();
+    const {
+        isLoading,
+        data: booking,
+        error,
+    } = useQuery({
+        queryKey: ["bookings"],
+        queryFn: () => getBooking(bookingId),
+    });
+
+    return { isLoading, error, booking };
+}
