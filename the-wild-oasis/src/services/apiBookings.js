f3a4540f8d9350829d73cf79be2900a76b95eb18
kacperjanus <kacperjanus8@gmail.com>
2023-08-21 22:49:54 +0200

    Added ability to sort bookings

--- the-wild-oasis/src/services/apiBookings.js
+++ the-wild-oasis/src/services/apiBookings.js
@@ -10,6 +10,9 @@ export async function getBookings({ filter, sortBy }) {
         query = query[filter.method || "eq"](filter.field, filter.value);
     }
 
+    if (sortBy)
+        query.order(sortBy.field, { ascending: sortBy.direction === "asc" });
+
     const { data, error } = await query;
     if (error) {
         console.error(error);
