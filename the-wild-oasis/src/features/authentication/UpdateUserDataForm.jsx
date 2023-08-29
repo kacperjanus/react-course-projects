4af58ab84bd912f272bfefa3404d0b75fce8094b
kacperjanus <kacperjanus8@gmail.com>
2023-08-29 22:06:35 +0200

    Implemented update account page

--- the-wild-oasis/src/features/authentication/UpdateUserDataForm.jsx
+++ the-wild-oasis/src/features/authentication/UpdateUserDataForm.jsx
@@ -7,51 +7,76 @@ import FormRow from "../../ui/FormRow";
 import Input from "../../ui/Input";
 
 import { useUser } from "./useUser";
+import { useUpdatedUser } from "./useUpdatedUser";
 
 function UpdateUserDataForm() {
-  // We don't need the loading state, and can immediately use the user data, because we know that it has already been loaded at this point
-  const {
-    user: {
-      email,
-      user_metadata: { fullName: currentFullName },
-    },
-  } = useUser();
+    // We don't need the loading state, and can immediately use the user data, because we know that it has already been loaded at this point
+    const {
+        user: {
+            email,
+            user_metadata: { fullName: currentFullName },
+        },
+    } = useUser();
 
-  const [fullName, setFullName] = useState(currentFullName);
-  const [avatar, setAvatar] = useState(null);
+    const { updateUser, isUpdatingUser } = useUpdatedUser();
 
-  function handleSubmit(e) {
-    e.preventDefault();
-  }
+    const [fullName, setFullName] = useState(currentFullName);
+    const [avatar, setAvatar] = useState(null);
 
-  return (
-    <Form onSubmit={handleSubmit}>
-      <FormRow label="Email address">
-        <Input value={email} disabled />
-      </FormRow>
-      <FormRow label="Full name">
-        <Input
-          type="text"
-          value={fullName}
-          onChange={(e) => setFullName(e.target.value)}
-          id="fullName"
-        />
-      </FormRow>
-      <FormRow label="Avatar image">
-        <FileInput
-          id="avatar"
-          accept="image/*"
-          onChange={(e) => setAvatar(e.target.files[0])}
-        />
-      </FormRow>
-      <FormRow>
-        <Button type="reset" variation="secondary">
-          Cancel
-        </Button>
-        <Button>Update account</Button>
-      </FormRow>
-    </Form>
-  );
+    function handleSubmit(e) {
+        e.preventDefault();
+        if (!fullName) return;
+        updateUser(
+            { fullName, avatar },
+            {
+                onSuccess: () => {
+                    setAvatar(null);
+                    e.target.reset();
+                },
+            }
+        );
+    }
+
+    function handleCancel() {
+        setFullName(currentFullName);
+        setAvatar(null);
+    }
+
+    return (
+        <Form onSubmit={handleSubmit}>
+            <FormRow label="Email address">
+                <Input value={email} disabled />
+            </FormRow>
+            <FormRow label="Full name">
+                <Input
+                    type="text"
+                    value={fullName}
+                    disabled={isUpdatingUser}
+                    onChange={(e) => setFullName(e.target.value)}
+                    id="fullName"
+                />
+            </FormRow>
+            <FormRow label="Avatar image">
+                <FileInput
+                    id="avatar"
+                    accept="image/*"
+                    disabled={isUpdatingUser}
+                    onChange={(e) => setAvatar(e.target.files[0])}
+                />
+            </FormRow>
+            <FormRow>
+                <Button
+                    disabled={isUpdatingUser}
+                    type="reset"
+                    variation="secondary"
+                    onClick={handleCancel}
+                >
+                    Cancel
+                </Button>
+                <Button disabled={isUpdatingUser}>Update account</Button>
+            </FormRow>
+        </Form>
+    );
 }
 
 export default UpdateUserDataForm;
