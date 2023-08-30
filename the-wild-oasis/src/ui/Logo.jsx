2e1671dbeda4083d7f00c1299cc451658a7aa691
kacperjanus <kacperjanus8@gmail.com>
2023-08-30 18:58:17 +0200

    Fixed colors of displayed Logo in dark mode

--- the-wild-oasis/src/ui/Logo.jsx
+++ the-wild-oasis/src/ui/Logo.jsx
@@ -13,7 +13,7 @@ const Img = styled.img`
 function Logo() {
     const { isDarkMode } = useDarkMode();
 
-    const src = isDarkMode ? "/logo-light.png" : "/logo-dark.png";
+    const src = isDarkMode ? "/logo-dark.png" : "/logo-light.png";
 
     return (
         <StyledLogo>
