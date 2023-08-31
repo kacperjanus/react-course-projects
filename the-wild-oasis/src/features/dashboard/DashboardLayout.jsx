ba3e576088c70bb99ecb0b4b8d81ed052def08af
kacperjanus <kacperjanus8@gmail.com>
2023-08-31 22:11:41 +0200

    Adding duration pie chart to dashboard page

--- the-wild-oasis/src/features/dashboard/DashboardLayout.jsx
+++ the-wild-oasis/src/features/dashboard/DashboardLayout.jsx
@@ -5,6 +5,7 @@ import { useRecentStays } from "./useRecentStays";
 import Stats from "./Stats";
 import { useCabins } from "../cabins/useCabins";
 import SalesChart from "./SalesChart";
+import DurationChart from "./DurationChart";
 
 const StyledDashboardLayout = styled.div`
     display: grid;
@@ -39,7 +40,7 @@ function DashboardLayout() {
                 cabinCount={cabins.length}
             />
             <div>Today's activity</div>
-            <div>Chart for stay durations</div>
+            <DurationChart confirmedStays={confirmedStays} />
             <SalesChart bookings={bookings} numDays={numDays} />
         </StyledDashboardLayout>
     );
