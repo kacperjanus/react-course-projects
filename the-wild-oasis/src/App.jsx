3b5b51d78d1366b9c5dffc83a6210881c5cd4ccc
kacperjanus <kacperjanus8@gmail.com>
2023-08-31 23:16:06 +0200

    Adding Error Boundary

--- the-wild-oasis/src/App.jsx
+++ the-wild-oasis/src/App.jsx
@@ -30,7 +30,6 @@ function App() {
         <DarkModeProvider>
             <QueryClientProvider client={queryClient}>
                 <ReactQueryDevtools initialIsOpen={false} />
-                <GlobalStyles />
                 <BrowserRouter>
                     <Routes>
                         <Route
