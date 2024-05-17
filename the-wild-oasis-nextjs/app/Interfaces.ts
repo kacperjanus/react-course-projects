618ea0ae08a01c842b8ffeccacf34695860130cd
kacperjanus <kacperjanus8@gmail.com>
2024-05-17 10:38:52 +0200

    Adding components from starter files

--- the-wild-oasis-nextjs/app/Interfaces.ts
+++ the-wild-oasis-nextjs/app/Interfaces.ts
@@ -1,4 +1,22 @@
 export interface Cabin {
     id: number;
     name: string;
+    maxCapacity: number;
+    regularPrice: number;
+    discount: number;
+    image: string
+}
+
+export interface Booking {
+    id: number;
+    guestId: number;
+    startDate: string;
+    endDate: string;
+    numNights: number;
+    totalPrice: number;
+    numGuests: number;
+    status: string;
+    created_at: string;
+    // cabins: { name, image },
+    cabins: { name: string, image: string };
 }
