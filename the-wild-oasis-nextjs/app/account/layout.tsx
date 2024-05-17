3ef39dd2b9ee76f87d840617ca8d422252a8fb46
kacperjanus <kacperjanus8@gmail.com>
2024-05-17 10:39:21 +0200

    Populating Account page

---
+++ the-wild-oasis-nextjs/app/account/layout.tsx
@@ -0,0 +1,13 @@
+import React, {ReactNode} from 'react';
+import SideNavigation from "@/app/_components/SideNavigation";
+
+function Layout({children}: { children: ReactNode }) {
+    return (
+        <div className="grid grid-cols-[16rem_1fr] h-full gap-12">
+            <div><SideNavigation/></div>
+            <div className="py-1">{children}</div>
+        </div>
+    );
+}
+
+export default Layout;
