049e3c42459b187b11f3ab67d8e8b2969ed77c8c
kacperjanus <kacperjanus8@gmail.com>
2024-05-18 07:40:45 +0200

    Populating Cabins page with remote data

---
+++ the-wild-oasis-nextjs/app/_components/CabinList.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import CabinCard from "@/app/_components/CabinCard";
+import {Cabin} from "@/app/Interfaces"
+import {getCabins} from "@/app/_lib/data-service";
+
+async function CabinList() {
+    const cabins: Cabin[] = await getCabins()
+
+    if (!cabins.length) {
+        return null
+    }
+
+    return (
+        <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
+            {cabins.map((cabin) => (
+                <CabinCard cabin={cabin} key={cabin.id}/>
+            ))}
+        </div>
+    )
+}
+
+export default CabinList;
--- the-wild-oasis-nextjs/app/cabins/page.tsx
+++ the-wild-oasis-nextjs/app/cabins/page.tsx
@@ -1,14 +1,12 @@
+import CabinList from "@/app/_components/CabinList";
+import {Suspense} from "react";
+import Spinner from "@/app/_components/Spinner";
+
 export const metadata = {
     title: "Cabins",
 }
 
-import CabinCard from "@/app/_components/CabinCard";
-import {Cabin} from "@/app/Interfaces"
-
 export default function Page() {
-    // CHANGE
-    const cabins: Cabin[] = [];
-
     return (
         <div>
             <h1 className="text-4xl mb-5 text-accent-400 font-medium">
@@ -23,13 +21,9 @@ export default function Page() {
                 to paradise.
             </p>
 
-            {cabins.length > 0 && (
-                <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
-                    {cabins.map((cabin) => (
-                        <CabinCard cabin={cabin} key={cabin.id}/>
-                    ))}
-                </div>
-            )}
+            <Suspense fallback={<Spinner/>}>
+                <CabinList/>
+            </Suspense>
         </div>
     );
 }
